2a9d725290c2ff040ef9d2e91c812fcc77d9ed66
Erik Prenzlin <eprenzlin@gmail.com>
2020-01-17 21:39:02 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,7 +43,7 @@
             sub_category:{ 
               label:"Species", 
               sub_category:{
-              nil
+              label: nil
                 } 
               }
             }
